aee098c66410b7514e72453dbe0b11d28cb6ecc6
MadiNithish <madinithish@gmail.com>
2026-06-04 12:57:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function insert_Row() {
     //Write your code here
-  const tr = document.createElement("tr");
-	tr.innerHTML = "<td>New Cell</td><td>New Cell2</td>"
-	const table = document.getElementById("sampleTable")
-	table.prepend(tr)
+
+	const table = document.getElementById("sampleTable");
+
+    const row = table.insertRow(0);
+
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
   
 }
